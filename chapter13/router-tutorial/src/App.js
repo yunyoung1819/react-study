e1226171931a8c8bbb853cf8ebb380587ba86bd6
yunyoung1819 <yunyoung1819@hanmail.net>
2020-12-06 02:40:52 +0900

    chapter13. react router study

--- chapter13/router-tutorial/src/App.js
+++ chapter13/router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
   return (
@@ -13,10 +14,17 @@ const App = () => {
           <li>
             <Link to="/about">소개</Link>
           </li>
+          <li>
+            <Link to="/profile/yunyoung1819">YUN YOUNG 프로필</Link>
+          </li>
+          <li>
+            <Link to="/profile/sweetjumi">JUMI 프로필</Link>
+          </li>
         </ul>
         <hr />
         <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profile/:username" component={Profile} />
       </div>
   );
 };
---
+++ chapter13/router-tutorial/src/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const data = {
+    yunyoung1819: {
+        name: '윤영',
+        description: '쿨하고 혁신적인 서비스를 만드는 소프트웨어 개발자'
+    },
+    sweetjumi: {
+        name: '이주미',
+        description: '어린이집 선생님'
+    }
+};
+
+const Profile = ({ match }) => {
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) {
+        return <div> 존재하지 않는 사용자 입니다. </div>;
+    }
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
