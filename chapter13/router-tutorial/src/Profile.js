489ee76e0945a357b96bf92c44e727882c9a9a28
yunyoung1819 <yunyoung1819@hanmail.net>
2020-12-14 23:35:06 +0900

    chapter13 router study

--- chapter13/router-tutorial/src/Profile.js
+++ chapter13/router-tutorial/src/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WithRouterSample from './WithRouterSample';
 
 const data = {
     yunyoung1819: {
@@ -23,6 +24,7 @@ const Profile = ({ match }) => {
                 {username}({profile.name})
             </h3>
             <p>{profile.description}</p>
+            <WithRouterSample/>
         </div>
     );
 };
