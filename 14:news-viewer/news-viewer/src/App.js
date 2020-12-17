16c66b6373aa1413d6211e1d284a3e6f873ff3bd
yunyoung1819 <yunyoung1819@hanmail.net>
2020-12-18 00:03:20 +0900

    remove 14:news-viewer/news-viewer

--- 14:news-viewer/news-viewer/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import NewsPage from './pages/NewsPage';
-
-const App = () => {
-    return <Route path="/:category?" component={NewsPage} />;
-};
-
-export default App;
