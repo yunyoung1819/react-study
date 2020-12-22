15a6b87ca33879bab514b1f72fbf9a1d38868900
yunyoung1819 <yunyoung1819@hanmail.net>
2020-12-23 01:03:53 +0900

    chapter09 react style study

--- chapter09/styling-react/config/webpack.config.js
+++ chapter09/styling-react/config/webpack.config.js
@@ -509,6 +509,7 @@ module.exports = function (webpackEnv) {
                     includePaths: [paths.appSrc + '/styles']
                   },
                   sourceMap: isEnvProduction && shouldUseSourceMap,
+                  prependData: `@import 'utils';`
                 }
               }),
               // Don't consider CSS imports dead code even if the
