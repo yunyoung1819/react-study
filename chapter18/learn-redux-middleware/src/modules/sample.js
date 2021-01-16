1bbeb17ceeb2ad28b7017de0282a5678001d5741
yunyoung1819 <yunyoung1819@hanmail.net>
2021-01-16 13:40:45 +0900

    react redux middleware study

--- chapter18/learn-redux-middleware/src/modules/sample.js
+++ chapter18/learn-redux-middleware/src/modules/sample.js
@@ -1,3 +1,15 @@
-import { handleActions } from 'redux-actions';
+import { handdleActions } from 'redux-actions';
 import * as api from '../lib/api';
 
+// 액션 타입을 선언합니다.
+// 한 요청당 세 개를 만들어야 합니다.
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USER_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+// thunk 함수를 생성합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
