a86db58bbdf7448734b17e4a526505e315238f30
yunyoung1819 <yunyoung1819@hanmail.net>
2021-04-17 21:29:42 +0900

    # 리액트 19장 코드스플리팅 스터디

--- chapter19/spitting-sample/src/App.js
+++ chapter19/spitting-sample/src/App.js
@@ -1,18 +1,24 @@
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
 function App() {
+    const [visible, setVisible] = useState(false);
     const onClick = () => {
-        import('./notify').then(result => result.default());
+        setVisible(true);
     }
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p onClick={onClick}>Hello React!</p>
+                <Suspense fallback={<div>loading...</div>}>
+                    {visible && <SplitMe />}
+                </Suspense>
             </header>
         </div>
-  );
+    );
 }
 
 export default App;
---
+++ chapter19/spitting-sample/src/SplitMe.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SplitMe = () => {
+    return <div>SplitMe</div>;
+};
+
+export default SplitMe;
+
